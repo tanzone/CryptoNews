9f25d9a9db87345cc92fa582226a2567dee908fb
Tanzone <Tanzone2000@gmail.com>
2022-05-20 11:00:07 +0200

    Card of coins with bad css. TODO css pls.

--- src/components/Pages/CriptosPage.jsx
+++ src/components/Pages/CriptosPage.jsx
@@ -7,9 +7,30 @@ import { useGetCryptosQuery } from '../../services/cryptoApi';
 
 const CriptosPage = () => {
   const { data: cryptosList, isFeatching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+  console.log(cryptos);
 
   return (
-    <div>CriptosPage</div>
+    <>
+      <Row gutters={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency) =>(
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={'/crypto/${currency.id}'}>
+              <Card 
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className='crypto-image' src={currency.iconUrl} />}
+                hoverable
+                >
+                  <p>Price: {millify(currency.price)}</p>
+                  <p>Market Cap: {millify(currency.marketCap)}</p>
+                  <p>Daily Change: {millify(currency.change)}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
